238a41c1b1c33c08ed61ecfbb5a77366899b4af3
Douglas R. Reno <renodr2002@gmail.com>
2024-11-06 19:57:26 -0600

    Select a random character from the characters.txt file and update the label.

--- Random Character Generator/MainWindow.xaml.cs
+++ Random Character Generator/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows;
 using System.IO;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Media.Animation;
+using System.Security.Cryptography;
+using System;
 
 namespace Random_Character_Generator
 {
@@ -20,8 +22,18 @@ namespace Random_Character_Generator
         {
             try
             {
+                // Read in the list of characters.
                 string[] characters = File.ReadAllLines(".\\characters.txt");
                 int NumberOfCharacters = characters.Length;
+
+                // Get a random number to select the character
+                Random character = new Random();
+                int character_number = character.Next(NumberOfCharacters);
+
+                // Set the character name.
+                string CharacterToPlay = characters.GetValue(character_number).ToString();
+
+                CharacterName.Content = CharacterToPlay;
             }
             catch (FileNotFoundException)
             {
